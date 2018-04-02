68620de393511a89c3440f3260360574f2ba3ac9
Sushant Aggarwal <sushant.aggarwal@markit.com>
2018-04-02 08:14:28 +0100

    updated label in plot 3.R

--- plot 3.R
+++ plot 3.R
@@ -16,5 +16,5 @@ unzip("data.zip")
 ## processing data
 NEI <- readRDS("summarySCC_PM25.rds")
 sub2 <- subset(NEI, fips == "24510")
-balt.sources <- aggregate(sub2[c("Emissions")], list(type = sub2$type, year = sub2$year), sum)
-qplot(year, Emissions, data = balt.sources, color = type, geom= "line")+ ggtitle("Total PM2.5 Emissions in Baltimore County by Source Type") + xlab("Year") + ylab("PM2.5 Emissions")
+baltimore.sources <- aggregate(sub2[c("Emissions")], list(type = sub2$type, year = sub2$year), sum)
+qplot(year, Emissions, data = baltimore.sources, color = type, geom= "line")+ ggtitle("Total PM2.5 Emissions in Baltimore County by Source Type") + xlab("Year") + ylab("PM2.5 Emissions")
